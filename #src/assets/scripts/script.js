fcdcbb0c4424419d366cede97fbd2fa68f2a0549
Hresa <gresapetro379@gmail.com>
2021-07-16 22:20:19 +0300

    add: footer and make adaptive and acardion

--- #src/assets/scripts/script.js
+++ #src/assets/scripts/script.js
@@ -1,51 +1,114 @@
 /*_Function  support browser webp css_*/
 
 @@include('main/_header-burger.js');
-
 @@include('vendors/owl.carousel.min.js');
 
 const querySell = x => document.querySelector(x);
-
 const leftArrow = '<div class="receipts__wrapArrow receipts__wrapArrow_left"> <img src = "./assets/images/svg/arrow-left.svg" alt="left" class="receipts__arrowLeft">Чоловічий <br> одяг</div>';
 const rightArrow = '<div class="receipts__wrapArrow receipts__wrapArrow_right">Жіночий <br> одяг <img src = "./assets/images/svg/arrow-right.svg" alt="right" class="receipts__arrowRight"></div>';
 
 $(document).ready(function(){
-    $('.receipts__carousel.owl-carousel').owlCarousel({
-        items:1,
-        dots: false,
-        autoplay: true,
-        nav: true,
-        // loop: true,
-        navText:[
-            leftArrow,
-            rightArrow
 
-        ]
-    });
+  $('.receipts__carousel.owl-carousel').owlCarousel({
+      items:1,
+      dots: false,
+      autoplay: true,
+      nav: true,
+      navText:[
+          leftArrow,
+          rightArrow
 
+      ]
   });
 
 
+  $('.reviewed__carousel.owl-carousel').owlCarousel({
+    items:2,
+    dots: false,
+    autoplay: true,
+    nav: false,
+    loop: true,
+    autoplayHoverPause:true,
+    smartSpeed:500,
+    autoplayTimeout:5000,
 
-window.addEventListener('resize' ,resizePage )
- 
+    responsive:{
+
+      0:{
+        items:1.2,
+        margin: 15,
+  
+
+      },
+
+      576:{
+        items:3,
+        margin: 15,
+        stagePadding: 0,
+      },
+
+      768:{
+        items:3,
+        margin: 20
+      },
+
+      992:{
+        items:4,
+        margin: 30
+      }
+
+
+    }
+
+  });
+
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+  window.addEventListener('resize' , debounce(resizePage,  11));
+
   resizePage()
-
+  
   function resizePage(){
+    console.log('1')
+    if(window.innerWidth < 576) querySell('.cardsBox').appendChild(querySell('.card.card__4'));
+    else querySell('.attention').appendChild( querySell('.card.card__4'));
 
-    if(window.innerWidth < 576) {
+    let footerDtat  =  document.querySelectorAll(".footer__date");
+    (window.innerWidth < 768)? footerDtat.forEach(item => item.open = false): footerDtat.forEach(item => item.open = true)
 
-            querySell('.cardsBox').appendChild( querySell('.card.card__4'))
-     
+    if(window.innerWidth < 400) querySell('.rights__wrapData').insertBefore(querySell('.rights__confidentiality'), querySell('.rights__protected') );
+    else querySell('.rights').appendChild( querySell('.rights__confidentiality'));
+  } 
+
+
+  // Function debonce
+  function debounce(callback , delay){
+    let timer;
+    return function(...args){
+      clearTimeout(timer);
+      timer = setTimeout(()=> {
+        callback.apply(this , args)
+      }, delay);
     }
-    else if(window.innerWidth > 576){
-
-         querySell('.attention').appendChild( querySell('.card.card__4'))
-    }
-
   }
 
 
+  // function throttle(callback , delay){
 
+  //   let isWaiting = false;
+
+  //   return function(...args){
+  //         if(isWaiting) return;
+  //         callback.apply(this , args);
+  //         isWaiting = true;
+
+  //         setTimeout(()=> isWaiting = false, delay)
+  //   }
+
+  // }
+
+// })
 
 
